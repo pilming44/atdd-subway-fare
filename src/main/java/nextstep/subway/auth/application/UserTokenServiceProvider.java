0afc9674f5707929b77c0afdd1446bfbb627202c
pilming44 <pilming44@naver.com>
2024-08-08 13:19:41 +0900

    리뷰반영) UserTokenServiceProvider 들여쓰기 개선

--- src/main/java/nextstep/subway/auth/application/UserTokenServiceProvider.java
+++ src/main/java/nextstep/subway/auth/application/UserTokenServiceProvider.java
@@ -12,12 +12,10 @@ public class UserTokenServiceProvider {
     private final List<UserTokenService> userTokenServices;
 
     public String createToken(UserTokenRequest userTokenRequest) {
-        for (UserTokenService userTokenService : userTokenServices) {
-            String token = userTokenService.createToken(userTokenRequest);
-            if (token != null) {
-                return token;
-            }
-        }
-        throw new IllegalArgumentException("요청에 적합한 UserTokenService가 없습니다.");
+        return userTokenServices.stream()
+                .map(service -> service.createToken(userTokenRequest))
+                .filter(token -> token != null)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("요청에 적합한 UserTokenService가 없습니다."));
     }
 }
